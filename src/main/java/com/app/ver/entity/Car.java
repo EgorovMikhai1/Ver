75a2788d0cb904ced64a2cbb30f7c1856d9b77fe
shonmikl <mikhailegorov676@gmail.com>
2025-01-27 13:57:13 +0100

    feat: create entities

---
+++ src/main/java/com/app/ver/entity/Car.java
@@ -0,0 +1,47 @@
+package com.app.ver.entity;
+
+import com.app.ver.entity.enums.Brand;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Getter
+@Setter
+public class Car {
+
+    private int id;
+
+    private String model;
+
+    private Brand brand;
+
+    private BigDecimal pricePerDay;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return id == car.id
+                && Objects.equals(model, car.model)
+                && brand == car.brand
+                && Objects.equals(pricePerDay, car.pricePerDay);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model, brand, pricePerDay);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", brand=" + brand +
+                ", pricePerDay=" + pricePerDay +
+                '}';
+    }
+}
---
+++ src/main/java/com/app/ver/entity/Rental.java
@@ -0,0 +1,41 @@
+package com.app.ver.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter
+@Setter
+public class Rental {
+
+    private int id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private BigDecimal totalCost;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rental rental = (Rental) o;
+        return id == rental.id && Objects.equals(startDate, rental.startDate) && Objects.equals(endDate, rental.endDate) && Objects.equals(totalCost, rental.totalCost);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, startDate, endDate, totalCost);
+    }
+
+    @Override
+    public String toString() {
+        return "Rental{" +
+                "id=" + id +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", totalCost=" + totalCost +
+                '}';
+    }
+}
