d08eae7716d8401a5a07d58ea8dc4496daba29f4
shonmikl <mikhailegorov676@gmail.com>
2025-02-10 13:49:41 +0100

    feat: add Validator

---
+++ src/main/java/com/app/ver/validation/IDCheckerImpl.java
@@ -0,0 +1,23 @@
+package com.app.ver.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.Optional;
+
+public class IDCheckerImpl implements ConstraintValidator<IDChecker, String> {
+    private static final String idPattern = "\\w+";
+
+    @Override
+    public void initialize(IDChecker constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return Optional.ofNullable(value)
+                .filter(id -> id.isBlank())
+                .map(id -> id.matches(idPattern))
+                .orElse(false);
+    }
+}
