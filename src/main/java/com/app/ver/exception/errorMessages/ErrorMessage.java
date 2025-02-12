4b93039ad462fbe9906243e48063c9a5d605dbcf
shonmikl <mikhailegorov676@gmail.com>
2025-02-12 14:02:31 +0100

    fix: check car brand in controller

--- src/main/java/com/app/ver/exception/errorMessages/ErrorMessage.java
+++ src/main/java/com/app/ver/exception/errorMessages/ErrorMessage.java
@@ -3,4 +3,6 @@ package com.app.ver.exception.errorMessages;
 public class ErrorMessage {
     public static final String CARS_NOT_EXIST_IN_DATABASE
             = "THERE_ARE_NOT_ANY_CARS_IN_DATABASE";
+    public static final String INCORRECT_BRAND_NAME
+            = "INCORRECT_BRAND_NAME";
 }
